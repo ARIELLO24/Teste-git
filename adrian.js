4fe307303ad707cf3c48a4d4f0b010921790f3e1
ARIELLO24 <ariellorenz24@gmail.com>
2024-10-21 20:18:15 -0300

    teste muito legal e maneiro

---
+++ adrian.js
@@ -0,0 +1,16 @@
+let ask = require("readline-sync")
+
+console.log("-----------------------ME DA A BUNDA-------------------------")
+
+let pergunta1 = ask.question("O Neymar é o melhor concorda?")
+
+
+if(pergunta1 === "sim") {
+console.log("obvio ne porra")
+
+}else{
+
+console.log("morre então")
+}
+
+
