71619381741052411c3654a2237060a3dcd75d2d
ARIELLO24 <ariellorenz24@gmail.com>
2024-10-21 21:37:57 -0300

    codigo js arrumado

---
+++ keller.js
@@ -0,0 +1,15 @@
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, idade, genero) {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+    }
+    return Pessoa;
+}());
+mostrarInfo();
+string;
+{
+    return "ola meu nome \u00E9 ".concat(this.nome, " tenho ").concat(this.idade, " e meu genero \u00E9 ").concat(this.genero);
+}
+var pessoa1 = new Pessoa("Arthur Manske", 16, "gay");
+console.log(pessoa1.mostrarInfo());
