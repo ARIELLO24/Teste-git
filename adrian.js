fd2ef457042b80c7b8beeff98a1349f951b0dfbf
ARIELLO24 <ariellorenz24@gmail.com>
2024-10-21 21:14:32 -0300

    teste 2 muito legal

--- adrian.js
+++ adrian.js
@@ -1,16 +1,27 @@
 let ask = require("readline-sync")
 
 console.log("-----------------------ME DA A BUNDA-------------------------")
-
+console.log("                                                             ")
 let pergunta1 = ask.question("O Neymar é o melhor concorda?")
 
 
-if(pergunta1 === "sim") {
+if(pergunta1 === "sim"){
 console.log("obvio ne porra")
-
 }else{
-
+console.clear()
 console.log("morre então")
 }
+
+let pergunta2 = ask.question("voce acha o cristiano gostoso?")
+
+
+if(pergunta2 === "sim"){
+console.log("maneirão mano")
+}else {
+console.log("ta de ladaia")
+}
+
+
+
 
 
